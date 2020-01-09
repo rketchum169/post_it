735f494c4c806d9eee487a6469ac6c069de1c7e4
Jeffery Gontarek <jeffery@jeffsmac.local>
2020-01-08 22:49:13 -0800

    Created basic structure for notes_controller 7 REST actions

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,2 +1,26 @@
 class NotesController < ApplicationController
-end
+
+  def index
+    @notes = Note.all
+  end
+
+  def show
+  end
+
+  def new
+    @note = Note.new
+  end
+
+  def edit
+  end
+
+  def create
+    @note = Note.new(note_params)
+  end
+
+  def update
+  end
+
+  def destroy
+    @note.destropy
+  end
